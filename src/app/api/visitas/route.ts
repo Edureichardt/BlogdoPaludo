75bdf25a59e131451fb4d9cf17c6bc8f43ae1125
Eduardo Reichardt <eduardoaugustin31@gmail.com>
2025-07-01 17:49:16 -0300

    123

--- src/app/api/visitas/route.ts
+++ src/app/api/visitas/route.ts
@@ -5,8 +5,10 @@ import { prisma } from '@/lib/prisma';
 export async function GET() {
   try {
     const totalVisitas = await prisma.visita.count();
+      console.log("Total de visitas no banco:", totalVisitas);
     return NextResponse.json({ total: totalVisitas });
   } catch (error) {
+    console.error("Erro ao contar visitas no banco:", error);
     console.error(error);
     return NextResponse.json({ error: "Erro interno no servidor" }, { status: 500 });
   }
