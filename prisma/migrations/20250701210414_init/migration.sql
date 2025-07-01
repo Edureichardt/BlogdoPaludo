0931db2907b3cbdf0b6f36c48650753e1eeb4271
Eduardo Reichardt <eduardoaugustin31@gmail.com>
2025-07-01 18:11:47 -0300

    dd

--- prisma/migrations/20250701210414_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Visita" (
-    "id" SERIAL NOT NULL,
-    "ip" TEXT NOT NULL,
-    "userAgent" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Visita_pkey" PRIMARY KEY ("id")
-);
