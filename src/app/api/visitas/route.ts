61a5e0eb4668ed5bf7cda94e92569d04463cfa2b
Eduardo Reichardt <eduardoaugustin31@gmail.com>
2025-07-02 08:42:51 -0300

    atualização metodo post

--- src/app/api/visitas/route.ts
+++ src/app/api/visitas/route.ts
@@ -9,14 +9,27 @@ export async function GET() {
     return NextResponse.json({ total: totalVisitas });
   } catch (error) {
     console.error("Erro ao contar visitas no banco:", error);
-
-    if (error instanceof Error) {
-      console.error("Mensagem:", error.message);
-      console.error("Stack:", error.stack);
-    } else {
-      console.error("Erro não é uma instância de Error:", error);
-    }
-
     return NextResponse.json({ error: "Erro interno no servidor" }, { status: 500 });
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const ip = req.headers.get("x-forwarded-for") || req.headers.get("x-real-ip") || "IP desconhecido";
+    const userAgent = req.headers.get("user-agent") || "User-Agent desconhecido";
+
+    await prisma.visita.create({
+      data: {
+        ip,
+        userAgent,
+      },
+    });
+
+    console.log("Nova visita registrada:", { ip, userAgent });
+
+    return NextResponse.json({ message: "Visita registrada com sucesso!" });
+  } catch (error) {
+    console.error("Erro ao registrar visita:", error);
+    return NextResponse.json({ error: "Erro ao registrar visita" }, { status: 500 });
   }
 }
